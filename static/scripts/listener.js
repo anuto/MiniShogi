0fe47da12b3d0d26908eef43228acf3fe2565b0a
althea poteet <altheapoteet@altheas-iMac.local>
2019-12-14 09:42:42 -0800

    Removed code that broke (not using redirect properly

--- static/scripts/listener.js
+++ static/scripts/listener.js
@@ -22,7 +22,6 @@ elems.forEach(function(elem) {
 
         var og = window.location.href
         window.location.href += "get_valid_moves/" + id
-        window.location.href = og
         selected = id
     });
 });
